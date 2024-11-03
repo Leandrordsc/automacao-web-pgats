ced0d5f0f0f28d4f57b658ff7a76b60754478c42
Leandrordsc <leandrocosta@brq.com>
2024-11-03 15:43:40 -0300

    Ajustantado_Results

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,21 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const path = require('path');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reportDir: 'cypress/results',
-  watchForFilesChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: 'https://automationexercise.com'
-  }
+    baseUrl: 'https://automationexercise.com',
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+    reportDir: path.join(__dirname, 'cypress/results'), 
+  },
+  watchForFilesChanges: false,
 });
-
